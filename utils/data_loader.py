c2d30bc1eb084b896a56e62617d2157f863f8561
evacharbit <126271793+evacharbit@users.noreply.github.com>
2025-12-11 20:22:13 +0100

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,33 +2,18 @@ import pandas as pd
 from config import PROJECT_ID, DATASET_ENRICHIE, client
 
 def load_personas_profiles():
-    query = f"""
-        SELECT *
-        FROM `{PROJECT_ID}.{DATASET_ENRICHIE}.personas_profiles`
-    """
-    return client.query(query).to_dataframe()
+    query = f"SELECT * FROM `{PROJECT_ID}.{DATASET_ENRICHIE}.personas_profiles`"
+    return client.query(query, location="EU").to_dataframe()  # 🔹 préciser la location
 
 def load_clusters():
-    query = f"""
-        SELECT *
-        FROM `{PROJECT_ID}.{DATASET_ENRICHIE}.cluster`
-    """
-    return client.query(query).to_dataframe()
+    query = f"SELECT * FROM `{PROJECT_ID}.{DATASET_ENRICHIE}.cluster`"
+    return client.query(query, location="EU").to_dataframe()
 
 def load_ticket():
-    query = f"""
-        SELECT *
-        FROM `{PROJECT_ID}.{DATASET_ENRICHIE}.ticket`
-    """
-    return client.query(query).to_dataframe()
+    query = f"SELECT * FROM `{PROJECT_ID}.{DATASET_ENRICHIE}.ticket`"
+    return client.query(query, location="EU").to_dataframe()
 
 def load_data():
-    """
-    Charge les trois tables essentielles :
-    - personas_profiles
-    - cluster
-    - ticket
-    """
     personas_profiles = load_personas_profiles()
     clusters = load_clusters()
     ticket = load_ticket()
